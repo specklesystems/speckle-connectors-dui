007794dae27cb3f8c2ec8e31f29c21f0311096b1
Dimitrie Stefanescu <didimitrie@gmail.com>
2025-05-30 15:24:41 +0100

    chore: just a comment

--- plugins/intercom.ts
+++ plugins/intercom.ts
@@ -94,7 +94,6 @@ export const useIntercom = () => {
         bootIntercom() // if active account changed and itercom is not initialised, do it
         return // we do not need to update, as that's done by default in the init
       }
-      // just update
       update({
         user_id: activeAccount.value.accountInfo.userInfo.id || '',
         name: activeAccount.value.accountInfo.userInfo.name,
