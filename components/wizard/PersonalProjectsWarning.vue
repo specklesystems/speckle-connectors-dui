6345ed2ded4b41a251d8e624f5092cb6142c9be8
oguzhankoral <oguzhankoral@gmail.com>
2025-06-17 12:55:50 +0300

    Change the messaging for personal projects

--- components/wizard/PersonalProjectsWarning.vue
+++ components/wizard/PersonalProjectsWarning.vue
@@ -12,78 +12,27 @@
               Move your projects to a workspace
             </h1>
             <p class="mb-2">
-              <span class="text-sm">➊</span>
               <span class="text-xs">
-                We are making workspaces the default way to work in Speckle.
-              </span>
-            </p>
-
-            <p class="mb-1">
-              <span class="text-sm">➋</span>
-              <span class="text-xs">
-                Introducing
-                <FormButton
-                  text
-                  link
-                  external
-                  size="sm"
-                  class="font-semibold"
-                  @click="$openUrl(`https://www.speckle.systems/pricing`)"
-                >
-                  new pricing
-                </FormButton>
-                including limits to the free plan.
+                Personal projects are being phased out. Move your projects to a
+                workspace to create new projects and models, invite new project
+                collaborators, and view comments and versions older than 7 days. By
+                January 1st 2026, all projects will be archived if not moved into a
+                workspace.
               </span>
             </p>
 
             <FormButton
-              text
               color="primary"
               size="sm"
-              :class="showMore ? `mb-1` : ``"
-              :icon-right="showMore ? ChevronUpIcon : ChevronDownIcon"
-              @click="showMore = !showMore"
+              class="mb-2"
+              @click="
+                $openUrl(
+                  `${accountStore.activeAccount.accountInfo.serverInfo.url}/projects`
+                )
+              "
             >
-              {{ showMore ? 'Show less' : 'Show timeline' }}
+              Move projects
             </FormButton>
-
-            <div v-show="showMore">
-              <hr />
-              <h3 class="font-medium text-warning-darker my-1">By June 1st 2025</h3>
-              <p class="text-xs mb-1">Move your projects to a workspace to:</p>
-              <ul class="list-disc list-inside pl-2 mb-2">
-                <li>
-                  <span class="text-xs font-medium">
-                    Create new projects and models
-                  </span>
-                  <span class="text-xs">
-                    (will be disabled for personal projects; existing projects and
-                    models stay editable)
-                  </span>
-                </li>
-                <li>
-                  <span class="text-xs font-medium">
-                    Invite new project collaborators
-                  </span>
-                  <span class="text-xs">
-                    (new invites will be unavailable for personal projects)
-                  </span>
-                </li>
-                <li>
-                  <span class="text-xs font-medium">
-                    Preserve version and comment history
-                  </span>
-                  <span class="text-xs">
-                    (history is reduced to 7 days for personal projects)
-                  </span>
-                </li>
-              </ul>
-              <h3 class="font-medium text-warning-darker">By Janury 1st 2026</h3>
-              <span class="text-xs mb-1">
-                All projects will be archived if not moved into a workspace. Don't
-                worry, we'll give you plenty of reminders before then.
-              </span>
-            </div>
           </div>
         </div>
       </div>
@@ -92,8 +41,8 @@
 </template>
 
 <script setup lang="ts">
-import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { useAccountStore } from '~/store/accounts'
 
+const accountStore = useAccountStore()
 const { $openUrl } = useNuxtApp()
-const showMore = ref(false)
 </script>
