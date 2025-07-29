18429421778e9840416e441dbbfcc5e67e971703
Björn <steinhagen.bjoern@gmail.com>
2025-07-29 10:37:54 +0200

    feat: basic structure

---
+++ pages/revit-mapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col space-y-2">
+    <div class="px-2 mt-2">
+      <FormButton to="/" size="sm" :icon-left="ArrowLeftIcon" class="my-2">
+        Home
+      </FormButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
+</script>
